9032783b4b75596867782b394092082960799c67
yuslianto <yuslianto56@gmail.com>
2021-04-13 11:36:17 +0700

    membuat navigasi/router pada project reactJS

--- src/pages/App.js
+++ src/pages/App.js
@@ -1,20 +1,11 @@
+import { Routes } from '../config';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Routes/>
       </header>
     </div>
   );
